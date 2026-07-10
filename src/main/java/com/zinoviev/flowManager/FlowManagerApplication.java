b911b6e01ddc405243b0f822cb5077e6101c4165
maxim zinovev <maximzin@mail.ru>
2026-07-10 19:57:40 +0500

    first commit

---
+++ src/main/java/com/zinoviev/flowManager/FlowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zinoviev.flowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zinoviev/flowManager/FlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinoviev.flowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
